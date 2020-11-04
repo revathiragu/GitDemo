631d0ae8c3f96f2d381fc1b4089e7e0c2fa49dad
revathiragu <revathimanju@gmail.com>
2020-11-04 12:52:28 +0530

    First Commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAppAndroid()
+	
+	{
+		System.out.println(" NativeAppAndroid");
+}
+	public void IosApp()
+	{
+		System.out.println(" Ios");
+	}
+}
